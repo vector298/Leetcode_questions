aa3411c752c6313b5e87c5b5f9b01becd96522b7
vector298 <ishaandasroy4000@gmail.com>
2026-07-09 13:59:57 +0530

    Time: 0 ms (100%), Space: 10.2 MB (71.42%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+vector<int> dp;
+int f(vector<int>& arr,int i){
+    if (i==arr.size()-1) return arr[i];
+    if(i==arr.size()-2) return max(arr[i],arr[i+1]);
+
+    return max(arr[i]+f(arr,i+2),0+f(arr,i+1));
+}
+int ftd(vector<int> &arr ,int i){
+    if (i==arr.size()-1) return arr[i];
+    if(i==arr.size()-2) return max(arr[i],arr[i+1]);
+
+    if(dp[i]!=-1) return dp[i];
+    
+        return dp[i]=max(arr[i]+ftd(arr,i+2),0+ftd(arr,i+1));
+    
+
+}
+
+    int rob(vector<int>& nums) {
+        dp.clear();
+        dp.resize(105,-1);
+        return ftd(nums,0);
+        
+    }
+};
